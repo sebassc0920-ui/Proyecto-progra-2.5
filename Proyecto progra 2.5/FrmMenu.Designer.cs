210941a4fc9fb445ee4f9a3b54c29da4e663f49a
sebassc0920-ui <sebassc0920@gmail.com>
2026-02-25 17:04:35 -0600

    Agregar archivos de proyecto.

---
+++ Proyecto progra 2.5/FrmMenu.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace Proyecto_progra_2._5
+{
+    partial class FrmMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.registroDeDatosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.seccionAdministrativaYPartePrivadaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mantenimientoDeEmpleadosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.acercaDeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.volverAlLoginToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loginToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.registroDeDatosToolStripMenuItem,
+            this.seccionAdministrativaYPartePrivadaToolStripMenuItem,
+            this.mantenimientoDeEmpleadosToolStripMenuItem,
+            this.acercaDeToolStripMenuItem,
+            this.volverAlLoginToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // registroDeDatosToolStripMenuItem
+            // 
+            this.registroDeDatosToolStripMenuItem.Name = "registroDeDatosToolStripMenuItem";
+            this.registroDeDatosToolStripMenuItem.Size = new System.Drawing.Size(111, 20);
+            this.registroDeDatosToolStripMenuItem.Text = "Registro de Datos";
+            // 
+            // seccionAdministrativaYPartePrivadaToolStripMenuItem
+            // 
+            this.seccionAdministrativaYPartePrivadaToolStripMenuItem.Name = "seccionAdministrativaYPartePrivadaToolStripMenuItem";
+            this.seccionAdministrativaYPartePrivadaToolStripMenuItem.Size = new System.Drawing.Size(221, 20);
+            this.seccionAdministrativaYPartePrivadaToolStripMenuItem.Text = "Seccion Administrativa y Parte Privada";
+            // 
+            // mantenimientoDeEmpleadosToolStripMenuItem
+            // 
+            this.mantenimientoDeEmpleadosToolStripMenuItem.Name = "mantenimientoDeEmpleadosToolStripMenuItem";
+            this.mantenimientoDeEmpleadosToolStripMenuItem.Size = new System.Drawing.Size(178, 20);
+            this.mantenimientoDeEmpleadosToolStripMenuItem.Text = "Mantenimiento de Empleados";
+            // 
+            // acercaDeToolStripMenuItem
+            // 
+            this.acercaDeToolStripMenuItem.Name = "acercaDeToolStripMenuItem";
+            this.acercaDeToolStripMenuItem.Size = new System.Drawing.Size(72, 20);
+            this.acercaDeToolStripMenuItem.Text = "Acerca De";
+            // 
+            // volverAlLoginToolStripMenuItem
+            // 
+            this.volverAlLoginToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.salirToolStripMenuItem,
+            this.loginToolStripMenuItem});
+            this.volverAlLoginToolStripMenuItem.Name = "volverAlLoginToolStripMenuItem";
+            this.volverAlLoginToolStripMenuItem.Size = new System.Drawing.Size(131, 20);
+            this.volverAlLoginToolStripMenuItem.Text = "Volver al Login o Salir";
+            // 
+            // salirToolStripMenuItem
+            // 
+            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.salirToolStripMenuItem.Text = "Salir";
+            this.salirToolStripMenuItem.Click += new System.EventHandler(this.salirToolStripMenuItem_Click);
+            // 
+            // loginToolStripMenuItem
+            // 
+            this.loginToolStripMenuItem.Name = "loginToolStripMenuItem";
+            this.loginToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.loginToolStripMenuItem.Text = "Login";
+            this.loginToolStripMenuItem.Click += new System.EventHandler(this.loginToolStripMenuItem_Click);
+            // 
+            // FrmMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "FrmMenu";
+            this.Text = "Menu";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem registroDeDatosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem seccionAdministrativaYPartePrivadaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mantenimientoDeEmpleadosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem acercaDeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem volverAlLoginToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loginToolStripMenuItem;
+    }
+}
---
+++ Proyecto progra 2.5/Login.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace Proyecto_progra_2._5
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNusuario = new System.Windows.Forms.TextBox();
+            this.txtTipoUsuario = new System.Windows.Forms.TextBox();
+            this.txtContraseña = new System.Windows.Forms.TextBox();
+            this.btnIngresar = new System.Windows.Forms.Button();
+            this.btnregistrar = new System.Windows.Forms.Button();
+            this.btncambiodecontraseña = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(147, 86);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Usuario";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(116, 168);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(135, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Tipo de Usuario";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(131, 251);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(102, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Contraseña";
+            // 
+            // txtNusuario
+            // 
+            this.txtNusuario.Location = new System.Drawing.Point(317, 85);
+            this.txtNusuario.Name = "txtNusuario";
+            this.txtNusuario.Size = new System.Drawing.Size(100, 20);
+            this.txtNusuario.TabIndex = 3;
+            // 
+            // txtTipoUsuario
+            // 
+            this.txtTipoUsuario.Location = new System.Drawing.Point(317, 170);
+            this.txtTipoUsuario.Name = "txtTipoUsuario";
+            this.txtTipoUsuario.Size = new System.Drawing.Size(100, 20);
+            this.txtTipoUsuario.TabIndex = 4;
+            // 
+            // txtContraseña
+            // 
+            this.txtContraseña.Location = new System.Drawing.Point(317, 251);
+            this.txtContraseña.Name = "txtContraseña";
+            this.txtContraseña.Size = new System.Drawing.Size(100, 20);
+            this.txtContraseña.TabIndex = 5;
+            // 
+            // btnIngresar
+            // 
+            this.btnIngresar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIngresar.Location = new System.Drawing.Point(549, 156);
+            this.btnIngresar.Name = "btnIngresar";
+            this.btnIngresar.Size = new System.Drawing.Size(102, 44);
+            this.btnIngresar.TabIndex = 6;
+            this.btnIngresar.Text = "Ingresar";
+            this.btnIngresar.UseVisualStyleBackColor = true;
+            // 
+            // btnregistrar
+            // 
+            this.btnregistrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnregistrar.Location = new System.Drawing.Point(549, 85);
+            this.btnregistrar.Name = "btnregistrar";
+            this.btnregistrar.Size = new System.Drawing.Size(100, 38);
+            this.btnregistrar.TabIndex = 7;
+            this.btnregistrar.Text = "Registrar";
+            this.btnregistrar.UseVisualStyleBackColor = true;
+            // 
+            // btncambiodecontraseña
+            // 
+            this.btncambiodecontraseña.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btncambiodecontraseña.Location = new System.Drawing.Point(549, 233);
+            this.btncambiodecontraseña.Name = "btncambiodecontraseña";
+            this.btncambiodecontraseña.Size = new System.Drawing.Size(216, 38);
+            this.btncambiodecontraseña.TabIndex = 8;
+            this.btncambiodecontraseña.Text = "Cambio de Contraseña";
+            this.btncambiodecontraseña.UseVisualStyleBackColor = true;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btncambiodecontraseña);
+            this.Controls.Add(this.btnregistrar);
+            this.Controls.Add(this.btnIngresar);
+            this.Controls.Add(this.txtContraseña);
+            this.Controls.Add(this.txtTipoUsuario);
+            this.Controls.Add(this.txtNusuario);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Login";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNusuario;
+        private System.Windows.Forms.TextBox txtTipoUsuario;
+        private System.Windows.Forms.TextBox txtContraseña;
+        private System.Windows.Forms.Button btnIngresar;
+        private System.Windows.Forms.Button btnregistrar;
+        private System.Windows.Forms.Button btncambiodecontraseña;
+    }
+}
+
