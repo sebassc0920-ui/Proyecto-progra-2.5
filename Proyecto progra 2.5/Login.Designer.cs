7756cd6aa2b1bde8c1ab2a20529d4fc0c2b1a9b1
Boni56 <eymibonilla71@gmail.com>
2026-02-25 17:21:37 -0600

    Se agrego boton de prueba

--- Proyecto progra 2.5/Login.Designer.cs
+++ Proyecto progra 2.5/Login.Designer.cs
@@ -37,15 +37,17 @@
             this.btnIngresar = new System.Windows.Forms.Button();
             this.btnregistrar = new System.Windows.Forms.Button();
             this.btncambiodecontraseña = new System.Windows.Forms.Button();
+            this.btnPRUEBA = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(147, 86);
+            this.label1.Location = new System.Drawing.Point(220, 132);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(71, 20);
+            this.label1.Size = new System.Drawing.Size(103, 29);
             this.label1.TabIndex = 0;
             this.label1.Text = "Usuario";
             // 
@@ -53,9 +55,10 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(116, 168);
+            this.label2.Location = new System.Drawing.Point(174, 258);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(135, 20);
+            this.label2.Size = new System.Drawing.Size(201, 29);
             this.label2.TabIndex = 1;
             this.label2.Text = "Tipo de Usuario";
             // 
@@ -63,39 +66,44 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(131, 251);
+            this.label3.Location = new System.Drawing.Point(196, 386);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(102, 20);
+            this.label3.Size = new System.Drawing.Size(146, 29);
             this.label3.TabIndex = 2;
             this.label3.Text = "Contraseña";
             // 
             // txtNusuario
             // 
-            this.txtNusuario.Location = new System.Drawing.Point(317, 85);
+            this.txtNusuario.Location = new System.Drawing.Point(476, 131);
+            this.txtNusuario.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtNusuario.Name = "txtNusuario";
-            this.txtNusuario.Size = new System.Drawing.Size(100, 20);
+            this.txtNusuario.Size = new System.Drawing.Size(148, 26);
             this.txtNusuario.TabIndex = 3;
             // 
             // txtTipoUsuario
             // 
-            this.txtTipoUsuario.Location = new System.Drawing.Point(317, 170);
+            this.txtTipoUsuario.Location = new System.Drawing.Point(476, 262);
+            this.txtTipoUsuario.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtTipoUsuario.Name = "txtTipoUsuario";
-            this.txtTipoUsuario.Size = new System.Drawing.Size(100, 20);
+            this.txtTipoUsuario.Size = new System.Drawing.Size(148, 26);
             this.txtTipoUsuario.TabIndex = 4;
             // 
             // txtContraseña
             // 
-            this.txtContraseña.Location = new System.Drawing.Point(317, 251);
+            this.txtContraseña.Location = new System.Drawing.Point(476, 386);
+            this.txtContraseña.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtContraseña.Name = "txtContraseña";
-            this.txtContraseña.Size = new System.Drawing.Size(100, 20);
+            this.txtContraseña.Size = new System.Drawing.Size(148, 26);
             this.txtContraseña.TabIndex = 5;
             // 
             // btnIngresar
             // 
             this.btnIngresar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnIngresar.Location = new System.Drawing.Point(549, 156);
+            this.btnIngresar.Location = new System.Drawing.Point(824, 240);
+            this.btnIngresar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnIngresar.Name = "btnIngresar";
-            this.btnIngresar.Size = new System.Drawing.Size(102, 44);
+            this.btnIngresar.Size = new System.Drawing.Size(153, 68);
             this.btnIngresar.TabIndex = 6;
             this.btnIngresar.Text = "Ingresar";
             this.btnIngresar.UseVisualStyleBackColor = true;
@@ -103,9 +111,10 @@
             // btnregistrar
             // 
             this.btnregistrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnregistrar.Location = new System.Drawing.Point(549, 85);
+            this.btnregistrar.Location = new System.Drawing.Point(824, 131);
+            this.btnregistrar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnregistrar.Name = "btnregistrar";
-            this.btnregistrar.Size = new System.Drawing.Size(100, 38);
+            this.btnregistrar.Size = new System.Drawing.Size(150, 58);
             this.btnregistrar.TabIndex = 7;
             this.btnregistrar.Text = "Registrar";
             this.btnregistrar.UseVisualStyleBackColor = true;
@@ -113,18 +122,29 @@
             // btncambiodecontraseña
             // 
             this.btncambiodecontraseña.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btncambiodecontraseña.Location = new System.Drawing.Point(549, 233);
+            this.btncambiodecontraseña.Location = new System.Drawing.Point(824, 358);
+            this.btncambiodecontraseña.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btncambiodecontraseña.Name = "btncambiodecontraseña";
-            this.btncambiodecontraseña.Size = new System.Drawing.Size(216, 38);
+            this.btncambiodecontraseña.Size = new System.Drawing.Size(324, 58);
             this.btncambiodecontraseña.TabIndex = 8;
             this.btncambiodecontraseña.Text = "Cambio de Contraseña";
             this.btncambiodecontraseña.UseVisualStyleBackColor = true;
             // 
+            // btnPRUEBA
+            // 
+            this.btnPRUEBA.Location = new System.Drawing.Point(548, 533);
+            this.btnPRUEBA.Name = "btnPRUEBA";
+            this.btnPRUEBA.Size = new System.Drawing.Size(128, 69);
+            this.btnPRUEBA.TabIndex = 9;
+            this.btnPRUEBA.Text = "button1";
+            this.btnPRUEBA.UseVisualStyleBackColor = true;
+            // 
             // Login
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1200, 692);
+            this.Controls.Add(this.btnPRUEBA);
             this.Controls.Add(this.btncambiodecontraseña);
             this.Controls.Add(this.btnregistrar);
             this.Controls.Add(this.btnIngresar);
@@ -134,6 +154,7 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Login";
             this.Text = "Login";
             this.ResumeLayout(false);
@@ -152,6 +173,7 @@
         private System.Windows.Forms.Button btnIngresar;
         private System.Windows.Forms.Button btnregistrar;
         private System.Windows.Forms.Button btncambiodecontraseña;
+        private System.Windows.Forms.Button btnPRUEBA;
     }
 }
 
